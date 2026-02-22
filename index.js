269f5f82f74548f53a58663d1779d84669a7628e
Tommymiza <tommymiza20@gmail.com>
2026-02-22 13:34:00 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hello Api!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
